27684761cf8d2503d0069a17410c25fab50f7628
mmalmberg95 <30853126+mmalmberg95@users.noreply.github.com>
2017-10-10 12:11:12 -0500

    Lots of things are happening

---
+++ app/src/main/java/com/malmberg/matthew/projectgo/BioData.java
@@ -0,0 +1,71 @@
+package com.malmberg.matthew.projectgo;
+
+/**
+ * Created by Matthew on 10/10/2017.
+ */
+
+public class BioData {
+    private String name;
+    private Integer price;
+    private String description;
+    private Boolean Eat;
+    private Boolean Drink;
+    private Boolean Listen;
+    private Boolean See;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getEat() {
+        return Eat;
+    }
+
+    public void setEat(Boolean eat) {
+        Eat = eat;
+    }
+
+    public Boolean getDrink() {
+        return Drink;
+    }
+
+    public void setDrink(Boolean drink) {
+        Drink = drink;
+    }
+
+    public Boolean getListen() {
+        return Listen;
+    }
+
+    public void setListen(Boolean listen) {
+        Listen = listen;
+    }
+
+    public Boolean getSee() {
+        return See;
+    }
+
+    public void setSee(Boolean see) {
+        See = see;
+    }
+}
--- app/src/main/java/com/malmberg/matthew/projectgo/OptionsList.java
+++ app/src/main/java/com/malmberg/matthew/projectgo/OptionsList.java
@@ -2,6 +2,15 @@ package com.malmberg.matthew.projectgo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OptionsList extends AppCompatActivity {
 
@@ -9,5 +18,32 @@ public class OptionsList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options_list);
+
+        readGoData();
+
+    }
+
+    private List<BioData> BiosList = new ArrayList<>();
+    private void readGoData() {
+        //takes GoData and puts it in an InputStream
+        InputStream is = getResources().openRawResource(R.raw.GoData);
+        //created to read the InputStream
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(is, Charset.forName("UTF-8"))
+        );
+    //loop to read file
+        String line = "";
+        try {
+            while ( (line = reader.readLine()) != null ) {
+
+            }
+        //catches any errors that might appear while reading the file and outputs the error message
+        } catch (IOException e) {
+            //wtf = What a Terrible Failure
+            Log.wtf("OptionsList", "Error reading file at line " + line, e);
+            e.printStackTrace();
+        }
+
+
     }
 }
